9b08c1b0a4575012c91dd87d23937504fef44db6
meetdhanani88 <meetdhanani87@gmail.com>
2022-07-20 12:04:47 +0530

    commnet remove

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import "./App.css";
 import Actionplan from "./Components/Actionplan";
-import Delete from "./Components/Delete";
-import Newplan from "./Components/Newplan";
-import SopAccess from "./Components/SopAccess";
 
 function App() {
   return (
     <div className="App">
-      {/* <Delete></Delete>
-      <Newplan></Newplan> */}
-      {/* <SopAccess></SopAccess> */}
       <Actionplan></Actionplan>
     </div>
   );
